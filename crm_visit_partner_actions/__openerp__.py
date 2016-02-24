d33e938522e17ddc4431062b4ee11c315ab0a00c
Andre Schenkels <a.schenkels@ictstudio.eu>
2016-02-24 10:59:30 +0000

    [ADD] Operating Unit Modules from Eficent -> Are in the process of going to the OCA so need update in the future
    git-svn-id: https://10.10.17.25/canna/odoo8-community/trunk@17 d910e14a-ac4a-4a9e-82b0-437c8ed1c4c9

---
+++ crm_visit_partner_actions/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2015 Onestein BV (www.onestein.eu).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'CRM Visit Partner Action - Intermediate module',
+    'version': '8.0.1.0.0',
+    'website' : 'https://www.onestein.eu',
+    'license': 'AGPL-3',
+    'category': 'CRM',
+    'summary': '',
+    'description': """
+CRM Visit Partner Actions
+=========================
+- Show and work with Partner Actions on Visits
+
+""",
+    'author': 'Onestein BV, André Schenkels',
+    'depends': [
+        'crm_visit',
+        'crm_partner_action'
+    ],
+    'data': [
+        'views/crm_visit.xml',
+    ],
+
+}
---
+++ crm_visit_partner_actions/models/crm_visit.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2015 Onestein BV (www.onestein.eu).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import logging
+from urlparse import urljoin
+from urllib import urlencode
+
+from openerp import models, fields, api, _
+from openerp.exceptions import Warning
+
+_logger = logging.getLogger(__name__)
+
+
+class CrmVisit(models.Model):
+    _inherit = "crm.visit"
+
+    partner_actions = fields.One2many(
+        related='partner_id.partner_actions',
+        string='Actions'
+    )
