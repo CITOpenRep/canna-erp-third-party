c0d6955a6a72234f55b37306ca14b20abc2d5f98
Kevin Graveman <k.graveman@onestein.nl>
2016-08-18 14:53:00 +0000

    [WIP] Sale discount script. Work in progress
    git-svn-id: https://10.10.17.25/canna/odoo8-community/trunk@109 d910e14a-ac4a-4a9e-82b0-437c8ed1c4c9

--- sale_discount_advanced/migrations/8.0.1.0.0/post-migration.py
+++ sale_discount_advanced/migrations/8.0.1.0.0/post-migration.py
@@ -28,9 +28,11 @@ logger = logging.getLogger('OpenUpgrade')
 
 def create_discounts(cr, env):
     discount_obj = env['sale.discount']
+    pricelist_obj = env['product.pricelist']
+    rule_obj = env['sale.discount.rule']
     legacy_product_pricelist_bulk = openupgrade.get_legacy_name(
         'product_pricelist_bulk')
-    legacy_discount = openupgrade.get_legacy_name('priority')
+    legacy_discount = openupgrade.get_legacy_name('discount')
     legacy_minimum_amount = openupgrade.get_legacy_name('minimum_amount')
     legacy_pricelist_id = openupgrade.get_legacy_name('pricelist_id')
 
@@ -49,14 +51,8 @@ def create_discounts(cr, env):
         # TODO make name depend on type of discount (e.g.
         # product.pricelist.bulk/payment_type/global_discount)
 
-        rule_vals = {
-            # 'company_id': ,
-            # 'sequence': ,
-            # 'sale_discount_id': , # Inverse of rules
-            'discount_type': 'perc',
-            'discount': bulk[1],
-            'max_base': bulk[2],
-        }
+        pricelist = pricelist_obj.search([('id', '=', bulk[3])])
+
         vals = {
             'id': bulk[0],
             # Assume empty table, so copy source ids should be safe.
@@ -66,10 +62,19 @@ def create_discounts(cr, env):
             'active': True,
             # 'product_id': ,
             'discount_base': 'sale_line',
-            'pricelists': (4, bulk[3], 0),
-            'rules': (0, 0, rule_vals),
+            'pricelists': (4, pricelist, 0),
+            'rules': (0, 0, rule_id),
         }
-        discount_obj.create(vals)
+        discount = discount_obj.create(vals)
+        rule_vals = {
+            # 'company_id': ,
+            # 'sequence': ,
+            'sale_discount_id': discount.id, # Inverse of rules
+            'discount_type': 'perc',
+            'discount': bulk[1],
+            'max_base': bulk[2],
+        }
+        rule_id = rule_obj.create(rule_vals)
 
 
 def add_discounts(cr, env):
@@ -82,21 +87,35 @@ def add_discounts(cr, env):
     Discount depends on pricelist too (field pricelist_id, whether the
     product is in the pricelist does not matter)
     """
-    order_obj = env.get('sale.order')
-    line_obj = env.get('sale.order.line')
-    discount_obj = env.get('sale.discount')
-    gdc_legacy = openupgrade.get_legacy_name('global_discount_confirm')
-    discount_legacy = openupgrade.get_legacy_name('discount')
+    # return  # TODO remove
+    order_obj = env['sale.order']
+    line_obj = env['sale.order.line']
+    discount_obj = env['sale.discount']
+    legacy_gdc = openupgrade.get_legacy_name('global_discount_confirm')
+    legacy_pt = openupgrade.get_legacy_name('payment_type')
+    legacy_discount = openupgrade.get_legacy_name('discount')
 
     minimum_discount = 1
-    orders = order_obj.search(cr, SUPERUSER_ID, [(gdc_legacy, '=', False)])
-    lines = line_obj.search(cr, SUPERUSER_ID, [
-        (discount_legacy, '>=', minimum_discount),
-        ('order_id', 'not in', orders)
+    # orders_gdc = order_obj.search([(legacy_gdc, '=', True)])
+    orders_global_discount = order_obj.search([(legacy_gdc, '=', True)])
+    lines = line_obj.search([
+        (legacy_discount, '>=', minimum_discount),
+        # ('order_id', 'not in', orders_gdc)
     ])
     for line in lines:
         # Note: Should not use line.discount as we should separate bulk and
-        # type disounts.
+        # type discounts.
+        discount_id = False
+        # pt_id = False
+        # pt_discounts = False
+        line.order_id.global_discount_confirm
+        openupgrade.logged_query(
+            cr,
+            """
+            SELECT {} FROM sale_order WHERE id = {}
+            """.format(legacy_gdc, line.order_id)
+        )
+
         if line.product_id.categ_id.apply_bulk_discount:
             highest_subtotal = 0
             minimum_amount = False
@@ -114,20 +133,88 @@ def add_discounts(cr, env):
             if minimum_amount:
                 # Get the relevant discount object
                 # Optimize: use xmlid?
-                discount_id = discount_obj.search(cr, SUPERUSER_ID, [
+                discount_id = discount_obj.search([
                     ('max_base', '=', minimum_amount),
                     ('discount', '=', discount_percentage)
                 ])
                 assert len(discount_id) == 1
-            sale_discounts = {'sale_discounts': (4, discount_id, 0)}
-            line.write(sale_discounts)
+            if discount_id:
+                sale_discounts = {'sale_discounts': (4, discount_id, 0)}
+                line.write(sale_discounts)
+        # TODO additional discounts based on payment type
+        # Get payment type discount.
+        # Have to get it through db, doesnt exist in V8.
+        openupgrade.logged_query(
+            cr,
+            """
+            SELECT payment_type FROM sale_order WHERE id = {}
+            """.format(line.order_id)
+        )
+        pt_id = cr.fetchall()
+        assert len(pt_id) <= 1
+        if pt_id:
+            openupgrade.logged_query(
+                cr,
+                """
+                SELECT {} FROM {} WHERE id = {} AND active = true
+                """.format(legacy_discount, legacy_pt, pt_id)
+            )
+            pt_discount = cr.fetchall()
+            # TODO DRY
+            # Find suitable discount
+            discount_id = discount_obj.search(
+                [('discount', '=', pt_discount)])
+            if not discount_id:
+                # Or create a new discount
+                rule_vals = {
+                    'discount_type': 'perc',
+                    'discount': pt_discount,
+                    'max_base': 0.0,
+                }
+                vals = {
+                    'name': '{} {}{}'.format('Discount',
+                                             pt_discount, '%'),
+                    'active': True,
+                    'discount_base': 'sale_line',
+                    'rules': (0, 0, rule_vals),
+                }
+                discount_id = discount_obj.create(vals)
+            # Add discount to the line
+            if discount_id:
+                sale_discounts = {'sale_discounts': (4, discount_id, 0)}
+                line.write(sale_discounts)
 
-        legacy_discounts = line.product_id.pricelist_id.bulk_order_discount_ids[
-            0]
-        # TODO case when there are multiple discounts
+        # Global discount override:
+        # Note that we cannot use else/elif because both
+        # global_discount_confirm and apply_bulk_discount can be true.
+        if line.order_id.global_discount_confirm:  # TODO Doesn't exist in v8
+            # Get global discount. Note that global discount also overrides payment
+            # type discount.
+            pass
+            # Find suitable discount
+            discount_id = discount_obj.search(
+                [('discount', '=', line.order_id.global_discount)])
+            if not discount_id:
+                # Or create a new discount
+                rule_vals = {
+                    'discount_type': 'perc',
+                    'discount': line.order_id.global_discount,
+                    'max_base': 0.0,
+                }
+                vals = {
+                    'name': '{} {}{}'.format('Discount', line.order_id.global_discount, '%'),
+                    'active': True,
+                    'discount_base': 'sale_line',
+                    'rules': (0, 0, rule_vals),
+                }
+                discount_id = discount_obj.create(vals)
+            # Add discount to the line
+            if discount_id:
+                sale_discounts = {'sale_discounts': (6, 0, [discount_id])}
+                line.write(sale_discounts)
 
-        # TODO Global discount override
-        # TODO additional discounts based on payment term
+        # legacy_discounts = line.product_id.pricelist_id.bulk_order_discount_ids[
+        #     0]
 
 
 @openupgrade.migrate(no_version=True)
