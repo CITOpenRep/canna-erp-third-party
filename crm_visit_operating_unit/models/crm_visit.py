e670a19d53a93c4fc680d3100fc0c2af4a3989e1
Kevin Graveman <k.graveman@onestein.nl>
2017-12-06 14:55:14 +0000

    Move operating_unit dynamic field domain and company property to operating_unit port to branch operating_unit.
    git-svn-id: https://10.10.17.25/canna/odoo8-community/trunk@366 d910e14a-ac4a-4a9e-82b0-437c8ed1c4c9
    
    Signed-off-by: Hiren Dangar <hiren.dangar.serpentcs@gmail.com>

--- crm_visit_operating_unit/models/crm_visit.py
+++ crm_visit_operating_unit/models/crm_visit.py
@@ -21,10 +21,8 @@
 ##############################################################################
 
 from openerp import fields, models, api
-from openerp.addons.operating_unit.models import ou_model
 
-
-class CrmVisit(ou_model.OUModel):
+class CrmVisit(models.Model):
     _inherit = 'crm.visit'
 
     operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit',
--- crm_visit_operating_unit/models/crm_visit_feeling.py
+++ crm_visit_operating_unit/models/crm_visit_feeling.py
@@ -21,10 +21,8 @@
 ##############################################################################
 
 from openerp import fields, models, api
-from openerp.addons.operating_unit.models import ou_model
 
-
-class CrmVisitFeeling(ou_model.OUModel):
+class CrmVisitFeeling(models.Model):
     _inherit = 'crm.visit.feeling'
 
     operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit',
