7afc9da042a5d786646238f0a63231ed1676f81f
Luc De Meyer <luc.demeyer@noviat.com>
2018-10-25 12:39:05 +0200

    bugfix + add chatter to discounts

--- sale_discount_advanced_payment_term/__openerp__.py
+++ sale_discount_advanced_payment_term/__openerp__.py
@@ -1,29 +1,11 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Odoo, Open Source Management Solution
-#
-#    Copyright (c) 2009-2016 Noviat nv/sa (www.noviat.com).
-#    Copyright (c) 2016 Onestein (www.onestein.eu).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright (C) 2016-2018 Noviat nv/sa (www.noviat.com).
+# Copyright (C) 2016 Onestein (http://www.onestein.eu/).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Sale Discount on Payment Terms',
-    'version': '8.0.1.0.0',
+    'version': '8.0.1.1.0',
     'license': 'AGPL-3',
     'author': 'Noviat,Onestein',
     'website': 'http://www.noviat.com',
--- sale_discount_advanced_payment_term/models/sale_discount.py
+++ sale_discount_advanced_payment_term/models/sale_discount.py
@@ -1,24 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Odoo, Open Source Management Solution
-#
-#    Copyright (c) 2009-2016 Noviat nv/sa (www.noviat.com).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright (C) 2016-2018 Noviat nv/sa (www.noviat.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import fields, models
 
@@ -30,4 +12,5 @@ class SaleDiscount(models.Model):
         comodel_name='account.payment.term',
         relation='payterm_sale_discount_rel',
         column1='discount_id',
-        column2='payterm_id')
+        column2='payterm_id',
+        track_visibility='onchange')
