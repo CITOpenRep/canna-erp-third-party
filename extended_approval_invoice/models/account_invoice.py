c1230ade3067f8a767e006fcf0f9fc703a387e71
s j <sj@1729.be>
2018-11-29 15:08:04 +0100

    cancel approval process add approval state on setup_complete

--- extended_approval_invoice/models/account_invoice.py
+++ extended_approval_invoice/models/account_invoice.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from openerp import models
+from openerp import api, models
 
 
 class AccountInvoice(models.Model):
@@ -8,3 +8,8 @@ class AccountInvoice(models.Model):
 
     workflow_signal = 'invoice_open'
     workflow_state = 'extended_approval'
+
+    @api.multi
+    def action_cancel(self):
+        self.cancel_approval()
+        return super(AccountInvoice, self).action_cancel()
