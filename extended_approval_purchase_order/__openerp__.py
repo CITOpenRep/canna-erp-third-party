45da45374742664996ae76d8c29cb256e00f2fcb
s j <sj@1729.be>
2018-11-29 12:52:38 +0100

    extended approval integration for purchase order

---
+++ extended_approval_purchase_order/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Extended Approval Purchase Order',
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Onestein',
+    'category': 'base',
+    'depends': [
+        'purchase',
+        'base_extended_approval',
+    ],
+    'data': [
+        'views/purchase_order.xml',
+    ],
+    'installable': True,
+}
