1daccce099f52c93ab02c8ddcda378040bf854b7
Luc de Meyer <luc.demeyer@noviat.com>
2019-09-11 17:25:36 +0200

    sale_discount_advanced 3.2.0

---
+++ sale_discount_advanced_pallet/README.rst
@@ -0,0 +1,50 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+============================================
+Pallet Matching Type Sale Discount Extension
+============================================
+
+This module extends the functionality of discount rules to allow matching
+on pallets, which are calculated based on the product packaging configuration.
+
+Installation
+============
+
+There is no specific installation procedure for this module.
+
+Configuration
+=============
+
+To configure this module, you need to:
+
+* Go to Sales > Configuration > Discounts
+
+  Select a rule for your discount and set the Matching type to "Pallet". 
+  You can now enter the Minimum and Maximum number of pallets.
+
+Usage
+=====
+
+If your conditions are met for your configured rules, the result of the
+rule is applied.
+
+Known issues
+============
+
+There no known issues at this point in time.
+
+Roadmap
+=======
+
+Addition or demo data and unit tests.
+
+Credits
+=======
+
+Contributors
+------------
+- Noviat, Luc De Meyer (noviat.com)
+- Dennis Sluijk <d.sluijk@onestein.nl> (onestein.nl)
+- Kevin Graveman <k.graveman@onestein.nl> (onestein.nl)
---
+++ sale_discount_advanced_pallet/models/sale_discount_rule.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Onestein (https://www.onestein.nl/).
+# Copyright 2019 Noviat (www.noviat.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models
+
+
+class SaleDiscountRule(models.Model):
+    _inherit = 'sale.discount.rule'
+
+    min_pallet = fields.Float(string='Minimum number of pallets')
+    max_pallet = fields.Float(string='Maximum number of pallets')
+
+    @api.model
+    def _selection_matching_type(self):
+        res = super(SaleDiscountRule, self)._selection_matching_type()
+        return res + [('pallet', 'Pallet')]
+
+    def _matching_type_methods(self):
+        methods = super(SaleDiscountRule, self)._matching_type_methods()
+        methods['pallet'] = '_pallet_matching'
+        return methods
+
+    @api.model
+    def _calculate_pallets(self, packaging, product_uom_qty):
+        total_on_pallet = (packaging.qty or 1) \
+            * (packaging.ul_qty or 1) \
+            * (packaging.rows or 1)
+        pallet_share = product_uom_qty / total_on_pallet
+        return pallet_share
+
+    @api.multi
+    def _compute_pallets(self, lines):
+        pallets = 0
+        for line in lines:
+            packaging = line.product_tmpl_id.packaging_ids
+            if not packaging:
+                continue
+            if len(packaging) > 1:
+                raise NotImplementedError(
+                    "Product {} (id: {}) has more than one packaging "
+                    "configuration.".format(
+                        line.product_tmpl_id.name, line.product_tmpl_id.id
+                    )
+                )
+            product_uom_qty = line.product_uom_qty
+            pallets += self._calculate_pallets(packaging[0], product_uom_qty)
+        return pallets
+
+    def _pallet_matching(self, lines):
+        self.ensure_one()
+        pallet_lines = self.env['sale.order.line']
+        for sol in lines:
+            if self.product_ids:
+                if sol.product_id not in self.product_ids:
+                    continue
+            elif self.product_category_ids:
+                if not any(sol.product_id._belongs_to_category(categ)
+                           for categ in self.product_category_ids):
+                    continue
+            pallet_lines += sol
+        computed_pallets = self._compute_pallets(pallet_lines)
+        if self.min_pallet > 0 and self.min_pallet > computed_pallets:
+            return False
+        if self.max_pallet > 0 and self.max_pallet < computed_pallets:
+            return False
+        return True
+
+    @api.onchange('matching_type')
+    def _onchange_matching_type(self):
+        if self.matching_type == 'pallet':
+            self.product_category_ids = False
+        super(SaleDiscountRule, self)._onchange_matching_type()
+
+    @api.depends('min_base', 'min_qty', 'min_pallet')
+    def _compute_min_view(self):
+        for rule in self:
+            if rule.matching_type == 'amount':
+                rule.min_view = rule.min_base
+            elif rule.matching_type == 'quantity':
+                rule.min_view = rule.min_qty
+            elif rule.matching_type == 'pallet':
+                rule.min_view = rule.min_pallet
+
+    @api.depends('max_base', 'max_qty', 'max_pallet')
+    def _compute_max_view(self):
+        for rule in self:
+            if rule.matching_type == 'amount':
+                rule.max_view = rule.max_base
+            elif rule.matching_type == 'quantity':
+                rule.max_view = rule.max_qty
+            elif rule.matching_type == 'pallet':
+                rule.max_view = rule.max_pallet
