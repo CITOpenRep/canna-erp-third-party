4e7e3b1356e8df30568266ada33e1e50f95543f8
Luc de Meyer <luc.demeyer@noviat.com>
2019-09-25 09:46:33 +0200

    Sale Discount Advanced 3.3.0

--- sale_discount_advanced_payment_term/models/sale_order.py
+++ sale_discount_advanced_payment_term/models/sale_order.py
@@ -11,4 +11,4 @@ class SaleOrder(models.Model):
     @api.onchange('payment_term')
     def _onchange_payment_term_id(self):
         for line in self.order_line:
-            line._onchange_sale_discount()
+            line.sale_discount_ids = line._get_sale_discounts()
