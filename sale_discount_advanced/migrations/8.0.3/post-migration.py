561a17a41598544923896758bfaeaf937cbf87d2
Kevin Graveman <k.graveman@onestein.nl>
2019-10-22 15:05:11 +0200

    fix sda migration script failing on sda versions below 2.1.0

--- sale_discount_advanced/migrations/8.0.3/post-migration.py
+++ sale_discount_advanced/migrations/8.0.3/post-migration.py
@@ -3,6 +3,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import logging
+from distutils.version import LooseVersion as lversion
+from openupgradelib import openupgrade
 
 from openerp import api, SUPERUSER_ID
 
@@ -10,7 +12,13 @@ _logger = logging.getLogger(__name__)
 _logger.setLevel(logging.DEBUG)
 
 
+@openupgrade.migrate(no_version=False)
 def migrate(cr, version):
+    if lversion(version) < lversion('8.0.2.1.0'):
+        # Field product_category_id was added in 2.1.0, so skip if older.
+        # For future reference: it was removed in version 3.1.0.
+        _logger.info("Skipping module as source version is %s.", version)
+        return
     env = api.Environment(cr, SUPERUSER_ID, {})
     rules = env['sale.discount.rule'].search([])
     for rule in rules:
