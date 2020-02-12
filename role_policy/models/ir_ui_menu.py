ba5691062a9c557f52992faa80833cdad1e414f1
Luc de Meyer <luc.demeyer@noviat.com>
2020-02-12 18:09:09 +0100

    Action bindings

--- role_policy/models/ir_ui_menu.py
+++ role_policy/models/ir_ui_menu.py
@@ -17,6 +17,15 @@ class IrUiMenu(models.Model):
 
     @api.model
     def _visible_menu_ids(self, debug=False):
+        """
+        TOOO:
+        We currently do not take into account yet roles on actions.
+        Hence an role on e.g. a window action will not remove the menu
+        entry like done for groups.
+        We could implement this via an extra filter in this method
+        or alternatively add/remove the 'role groups' to action when we
+        add/remove roles on actions.
+        """
         visible_ids = super()._visible_menu_ids(debug=debug)
         menus = self.browse(visible_ids)
         roles = self.env.user.role_ids
