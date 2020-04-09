88744b96642524da1e913a88a17849b8f1064bd4
Luc de Meyer <luc.demeyer@noviat.com>
2020-04-09 22:07:44 +0200

    Price catalog ou

---
+++ price_catalog_operating_unit/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 Onestein B.V.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Price Catalog Operating Unit",
+    "version": "13.0.1.0.0",
+    "category": "Sales",
+    "website": "https://onestein.nl/",
+    "author": "Onestein B.V.",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["price_catalog", "operating_unit"],
+    "data": [
+        "views/price_subcatalog_views.xml",
+    ],
+}
