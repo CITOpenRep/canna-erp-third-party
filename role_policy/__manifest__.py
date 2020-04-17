92ab2de8abfcb41e0ddc4083f242f87447842b56
Luc De Meyer <luc.demeyer@noviat.com>
2020-04-17 16:17:24 +0200

    [13.0][IMP]role policy window actions view

--- role_policy/__manifest__.py
+++ role_policy/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Role Policy",
-    "version": "13.0.0.6.4",
+    "version": "13.0.0.6.5",
     "license": "AGPL-3",
     "author": "Noviat",
     "website": "http://www.noviat.com",
