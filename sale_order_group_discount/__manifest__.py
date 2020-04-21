a628fe6176647cca6b9fadac84c85dcd1e0787df
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2020-04-21 16:38:28 +0530

    [MIG]sale_order_group_discount: Migrated sale_order_group_discount module in v13.

---
+++ sale_order_group_discount/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2019 Noviat.
+# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Order Group Discount",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Noviat. " "Serpent Consulting Services Pvt. Ltd.",
+    "category": "Sales",
+    "depends": ["sale_order_group", "sale_discount_advanced",],
+    "data": ["views/sale_order_group_views.xml",],
+    "installable": True,
+}
