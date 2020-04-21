9ea7f8303fda091c641a3901dc2edccd4668189a
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2020-04-21 15:48:08 +0530

    [MIG]sale_discount_advanced_extensions_demo: Migrated sale_discount_advanced_extensions_demo module in v13.

---
+++ sale_discount_advanced_extensions_demo/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2019 Noviat nv/sa (www.noviat.com).
+# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Discount Advanced Extensions Demo",
+    "author": "Noviat, " "Serpent Consulting Services Pvt. Ltd.",
+    "category": "CES",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale_discount_advanced",],
+}
--- sale_discount_advanced_extensions_demo/models/sale_discount_rule.py
+++ sale_discount_advanced_extensions_demo/models/sale_discount_rule.py
@@ -1,45 +1,37 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2019 Noviat nv/sa (www.noviat.com).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-import logging
-
-from openerp import api, models
-
-_logger = logging.getLogger(__name__)
+from odoo import api, models
 
 
 class SaleDiscountRule(models.Model):
-    _inherit = 'sale.discount.rule'
+    _inherit = "sale.discount.rule"
 
     @api.model
     def _selection_matching_type(self):
         res = super(SaleDiscountRule, self)._selection_matching_type()
-        return res + [('pallet', 'Pallet')]
+        return res + [("pallet", "Pallet")]
 
     @api.model
     def _selection_matching_extra(self):
         res = super(SaleDiscountRule, self)._selection_matching_extra()
-        return res + [('extra', 'Extra')]
+        return res + [("extra", "Extra")]
 
     def _matching_type_methods(self):
         methods = super(SaleDiscountRule, self)._matching_type_methods()
-        methods['pallet'] = '_pallet_matching'
+        methods["pallet"] = "_pallet_matching"
         return methods
 
     def _matching_extra_methods(self):
         methods = super(SaleDiscountRule, self)._matching_extra_methods()
-        methods['extra'] = '_extra_matching'
+        methods["extra"] = "_extra_matching"
         return methods
 
     def _pallet_matching(self, lines):
-        _logger.warn(
-            '_pallet_matching for rule %s, lines=%s',
-            self, lines)
+        _logger.warn("_pallet_matching for rule %s, lines=%s", self, lines)
         return True
 
     def _extra_matching(self, lines):
-        _logger.warn(
-            '_extra_matching for rule %s, lines=%s',
-            self, lines)
+        _logger.warn("_extra_matching for rule %s, lines=%s", self, lines)
         return True
