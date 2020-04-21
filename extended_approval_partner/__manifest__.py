75b9d26a074fa25ed80ac87090d0edc70ff9b669
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2020-04-21 12:32:04 +0530

    [MIG]extended_approval_partner: Migrated extended_approval_partner module in v13.

---
+++ extended_approval_partner/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Extended Approval Partners",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Onestein, " "Serpent Consulting Services Pvt. Ltd.",
+    "category": "base",
+    "depends": ["base", "base_extended_approval",],
+    "data": [
+        "data/email_template_data.xml",
+        "data/base_action_rule_data.xml",
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+}
--- extended_approval_partner/models/res_partner.py
+++ extended_approval_partner/models/res_partner.py
@@ -1,37 +1,31 @@
-# -*- coding: utf-8 -*-
-from openerp import api, fields, models
+# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    _name = 'res.partner'
-    _inherit = ['res.partner', 'extended.approval.mixin']
+    _name = "res.partner"
+    _inherit = ["res.partner", "extended.approval.mixin"]
 
-    workflow_state_field = 'state'
-    workflow_state = 'extended_approval'
-    workflow_start_state = 'draft'
+    workflow_state_field = "state"
+    workflow_state = "extended_approval"
+    workflow_start_state = "draft"
 
     state = fields.Selection(
         selection=[
-            ('draft', 'Draft'),
-            ('extended_approval', 'Approval'),
-            ('confirmed', 'Confirmed')
+            ("draft", "Draft"),
+            ("extended_approval", "Approval"),
+            ("confirmed", "Confirmed"),
         ],
         default="draft",
-        string='State')
+        string="State",
+    )
 
-    @api.multi
     def set_state_to_confirmed(self):
-        r = self.approve_step()
-        if r or self.current_step:
-            setattr(
-                self,
-                self.workflow_state_field,
-                self.workflow_state)
-            return r
-        else:
-            self.state = 'confirmed'
+        self.approve_step()
+        self.state = "confirmed"
 
-    @api.multi
     def reset_to_draft(self):
         self.cancel_approval()
-        self.state = 'draft'
+        self.state = "draft"
