8ee790d8b27e5317556011ec893d55f2e026e6ec
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2020-04-22 11:07:34 +0530

    [MIG]product_stock_view: Migrated product_stock_view module in v13.

---
+++ product_stock_view/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2009-2017 Noviat nv/sa (www.noviat.com).
+# Copyright (c) 2016-2017 Onestein BV (www.onestein.eu).
+# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Stock View",
+    "version": "13.0.1.0.0",
+    "website": "https://www.onestein.eu",
+    "license": "AGPL-3",
+    "category": "Custom",
+    "summary": "Product Stock View",
+    "author": "Onestein BV,Noviat, " "Serpent Consulting Services Pvt. Ltd.",
+    "depends": ["product", "purchase", "stock",],
+    "data": ["views/purchase_view.xml",],
+    "installable": True,
+}
