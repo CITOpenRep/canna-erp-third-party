4d1ce402d1ce06d2395670ebd21403edff998e98
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2020-04-22 10:21:59 +0530

    [MIG]crm_visit_partner_actions: Migrated crm_visit_partner_actions module in v13.

---
+++ crm_visit_partner_actions/__manifest__.py
@@ -0,0 +1,21 @@
+# OpenERP, Open Source Management Solution
+# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "CRM Visit Partner Action - Intermediate module",
+    "version": "13.0.1.0.0",
+    "website": "https://www.onestein.eu",
+    "license": "AGPL-3",
+    "category": "CRM",
+    "summary": "",
+    "description": """
+CRM Visit Partner Actions
+=========================
+- Show and work with Partner Actions on Visits
+
+""",
+    "author": "Onestein BV, André Schenkels, " "Serpent Consulting Services Pvt. Ltd.",
+    "depends": ["crm_visit", "crm_partner_action"],
+    "data": ["views/crm_visit.xml",],
+}
--- crm_visit_partner_actions/models/crm_visit.py
+++ crm_visit_partner_actions/models/crm_visit.py
@@ -1,39 +1,11 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#
-#    Copyright (c) 2015 Onestein BV (www.onestein.eu).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# OpenERP, Open Source Management Solution
+# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-import logging
-from urlparse import urljoin
-from urllib import urlencode
-
-from openerp import models, fields, api, _
-from openerp.exceptions import Warning
-
-_logger = logging.getLogger(__name__)
+from odoo import fields, models
 
 
 class CrmVisit(models.Model):
     _inherit = "crm.visit"
 
-    partner_actions = fields.One2many(
-        related='partner_id.actions',
-        string='Actions'
-    )
+    partner_actions = fields.One2many(related="partner_id.actions", string="Actions")
