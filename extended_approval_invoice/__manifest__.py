13bcc099b0d7f3fab3a87c95cf7aa0217ef0e03f
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2020-04-27 13:12:05 +0530

    [IMP]Improved code as par oca guidelines.

--- extended_approval_invoice/__manifest__.py
+++ extended_approval_invoice/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# Copyright (C) 2020-TODAY SerpentCS Pvt. Ltd. (<http://www.serpentcs.com>).
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
@@ -7,7 +7,7 @@
     "license": "AGPL-3",
     "author": "Onestein, " "Serpent Consulting Services Pvt. Ltd.",
     "category": "base",
-    "depends": ["account", "base_extended_approval",],
-    "data": ["views/account_invoice.xml",],
+    "depends": ["account", "base_extended_approval"],
+    "data": ["views/account_invoice.xml"],
     "installable": True,
 }
