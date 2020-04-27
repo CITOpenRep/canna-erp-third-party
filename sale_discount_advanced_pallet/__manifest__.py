9af62b802e3843475590712c7340cbe3b887ef4a
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2020-04-27 11:58:37 +0530

    [IMP]Improved code as par oca guidelines.

--- sale_discount_advanced_pallet/__manifest__.py
+++ sale_discount_advanced_pallet/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright 2019 Onestein (https://www.onestein.nl/).
 # Copyright 2019 Noviat (www.noviat.com).
-# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# Copyright (C) 2020-TODAY SerpentCS Pvt. Ltd. (<http://www.serpentcs.com>).
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
@@ -11,6 +11,6 @@
     "category": "Sales",
     "version": "13.0.1.0.0",
     "license": "AGPL-3",
-    "depends": ["sale_discount_advanced",],
-    "data": ["views/sale_discount_views.xml",],
+    "depends": ["sale_discount_advanced"],
+    "data": ["views/sale_discount_views.xml"],
 }
--- sale_discount_advanced_pallet/models/sale_discount_rule.py
+++ sale_discount_advanced_pallet/models/sale_discount_rule.py
@@ -1,6 +1,6 @@
 # Copyright 2019 Onestein (https://www.onestein.nl/).
 # Copyright 2019 Noviat (www.noviat.com).
-# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# Copyright (C) 2020-TODAY SerpentCS Pvt. Ltd. (<http://www.serpentcs.com>).
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
