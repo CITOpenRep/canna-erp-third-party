fa5fb741f50153cdcd3661f907f372b56a829c10
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2020-04-27 13:24:21 +0530

    [IMP]Improved code as par oca guidelines.

--- extended_approval_purchase_order/__init__.py
+++ extended_approval_purchase_order/__init__.py
@@ -1,4 +1,3 @@
-# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import models
--- extended_approval_purchase_order/models/purchase_order.py
+++ extended_approval_purchase_order/models/purchase_order.py
@@ -1,7 +1,7 @@
-# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# Copyright (C) 2020-TODAY SerpentCS Pvt. Ltd. (<http://www.serpentcs.com>).
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class PurchaseOrder(models.Model):
