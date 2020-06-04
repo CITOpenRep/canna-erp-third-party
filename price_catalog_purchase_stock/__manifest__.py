d69630b5e847a317c3716ac3422cb8c1b9e737c0
hughes damry <noviat-hughes@canna.com>
2020-06-04 08:39:21 +0200

    13.0 add price catalog purchase stock

---
+++ price_catalog_purchase_stock/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Noviat.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Price Catalog Purchase Stock",
+    "summary": "Price Catalog Logic for Automatic Purchases Orders",
+    "version": "13.0.1.0.0",
+    "category": "Operations/Purchase",
+    "website": "https://www.noviat.com/",
+    "author": "Noviat",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["purchase_stock", "price_catalog_purchase"],
+}
