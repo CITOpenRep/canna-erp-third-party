0ec1d1807e481e3138364ba16cedd72bbf3d49db
Luc De Meyer <luc.demeyer@noviat.com>
2020-09-29 18:43:23 +0200

    [13.0][ADD]account_journal_lock_date_nov

---
+++ account_journal_lock_date_nov/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2019-2020 Noviat.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Journal Lock Date",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Noviat",
+    "website": "http://www.noviat.com",
+    "category": "Accounting & Finance",
+    "depends": ["account"],
+    "data": ["views/account_journal_views.xml"],
+    "installable": True,
+}
