9a458742dbdf669cd62a294d0e5080e3ef5f874f
Luc De Meyer <luc.demeyer@noviat.com>
2020-10-15 19:56:50 +0200

    [MIG] : Migration to 13.0

--- sale_order_group_invoice/models/sale_order_group.py
+++ sale_order_group_invoice/models/sale_order_group.py
@@ -1,61 +1,52 @@
-# -*- coding: utf-8 -*-
-# Copyright 2019 Noviat.
+# Copyright 2019-2020 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from openerp import api, fields, models, _
+from odoo import _, api, fields, models, tools
 
 
 class SaleOrderGroup(models.Model):
-    _inherit = 'sale.order.group'
+    _inherit = "sale.order.group"
 
-    disable_invoice_create = fields.Boolean(
-        compute='_compute_invoice_flags')
-    view_invoices_invisible = fields.Boolean(
-        compute='_compute_invoice_flags')
+    disable_invoice_create = fields.Boolean(compute="_compute_invoice_flags")
+    view_invoices_invisible = fields.Boolean(compute="_compute_invoice_flags")
 
-    @api.multi
-    @api.depends('sale_order_ids')
+    @api.depends("sale_order_ids")
     def _compute_invoice_flags(self):
         for sog in self:
             sog.view_invoices_invisible = True
             sog.disable_invoice_create = True
-            states = list(set(sog.sale_order_ids.mapped('state')))
-            if len(states) == 1 and states[0] == 'manual':
-                if len(sog.sale_order_ids.mapped('currency_id')) == 1:
-                    sog.disable_invoice_create = False
-            invoices = sog.sale_order_ids.mapped('invoice_ids')
+            invoice_states = list(set(sog.sale_order_ids.mapped("invoice_status")))
+            if any(state == "to invoice" for state in invoice_states):
+                sog.disable_invoice_create = False
+            invoices = sog.sale_order_ids.mapped("invoice_ids")
             if invoices:
                 sog.view_invoices_invisible = False
-                invoice_states = invoices.mapped('state')
-                if 'open' in invoice_states or 'paid' in invoice_states:
+                invoice_states = invoices.mapped("state")
+                if "open" in invoice_states or "paid" in invoice_states:
                     sog.disable_invoice_create = True
 
-    @api.multi
     def create_invoice(self):
         self.ensure_one()
-        ctx = dict(
-            self.env.context,
-            default_sale_order_group_id=self.id)
+        ctx = dict(self.env.context, default_sale_order_group_id=self.id)
         return {
-            'name': _("Create Invoice"),
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'sale.order.group.invoice.create',
-            'view_id': False,
-            'type': 'ir.actions.act_window',
-            'context': ctx,
-            'target': 'new',
+            "name": _("Create Invoice"),
+            "view_type": "form",
+            "view_mode": "form",
+            "res_model": "sale.order.group.invoice.create",
+            "view_id": False,
+            "type": "ir.actions.act_window",
+            "context": ctx,
+            "target": "new",
         }
 
-    @api.multi
     def view_invoices(self):
         self.ensure_one()
-        invoices = self.sale_order_ids.mapped('invoice_ids')
-
-        action = self.env['ir.actions.act_window'].for_xml_id(
-            'account', 'action_invoice_tree1')
-        domain = eval(action.get('domain') or '[]')
-        domain += [('id', 'in', invoices.ids)]
-        action.update({'domain': domain})
+        invoices = self.sale_order_ids.mapped("invoice_ids")
+        action = self.env["ir.actions.act_window"].for_xml_id(
+            "account", "action_move_out_invoice_type"
+        )
+        domain = tools.safe_eval(action.get("domain") or "[]")
+        domain += [("id", "in", invoices.ids)]
+        action.update({"domain": domain})
         return action
--- sale_order_group_invoice/wizards/sale_order_group_invoice_create.py
+++ sale_order_group_invoice/wizards/sale_order_group_invoice_create.py
@@ -1,32 +1,21 @@
-# -*- coding: utf-8 -*-
-# Copyright 2019 Noviat.
+# Copyright 2019-2020 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models, _
-from openerp.exceptions import Warning as UserError
+from odoo import fields, models
 
 
 class SaleOrderGroupInvoiceCreate(models.TransientModel):
-    _name = 'sale.order.group.invoice.create'
-    _description = 'Create Sale Order Group Invoice'
+    _name = "sale.order.group.invoice.create"
+    _description = "Create Sale Order Group Invoice"
 
     sale_order_group_id = fields.Many2one(
-        comodel_name='sale.order.group',
-        string='Sale Order Group',
-        readonly=True)
-    keep_references = fields.Boolean(
-        string='Keep references from original invoices', default=True)
-    date_invoice = fields.Date(string='Invoice Date')
+        comodel_name="sale.order.group", string="Sale Order Group", readonly=True
+    )
+    date_invoice = fields.Date(string="Invoice Date")
 
-    @api.multi
     def create_invoice(self):
-        self.ensure_one()
-        orders = self.sale_order_group_id.sale_order_ids
-        try:
-            orders.signal_workflow('manual_invoice')
-            invoices = orders.mapped('invoice_ids')
-            invoices.do_merge(keep_references=self.keep_references,
-                              date_invoice=self.date_invoice)
-        except Exception:
-            raise UserError(_("Invoice creation failed"))
-        return True
+        ctx = dict(self.env.context, date_invoice=self.date_invoice)
+        for sog in self:
+            orders = sog.with_context(ctx).sale_order_group_id.sale_order_ids
+            invoices = orders._create_invoices()
+        return invoices
