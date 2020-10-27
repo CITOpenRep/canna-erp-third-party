9136519e79c6a4a7986657bbd08460a2db15abcc
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2020-10-27 10:57:48 +0530

    [MIG] email_template_inactive: Migrated email_template_inactive in v13

--- email_template_inactive/__manifest__.py
+++ email_template_inactive/__manifest__.py
@@ -1,15 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2009-2017 Noviat.
+# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd.
+#    (<http://www.serpentcs.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Email Template Inactive',
     'summary': 'Add active flag to Email Templates',
     'author': 'Noviat',
     'category': 'Social Network',
-    'version': '8.0.1.0.0',
+    'version': '13.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
-        'email_template',
+        'mail',
     ],
     'data': [
         'views/email_template.xml',
--- email_template_inactive/models/email_template.py
+++ email_template_inactive/models/email_template.py
@@ -1,7 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2009-2017 Noviat.
+# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd.
+#    (<http://www.serpentcs.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from openerp import fields, models
+from odoo import fields, models
 
 
 class EmailTemplate(models.Model):
