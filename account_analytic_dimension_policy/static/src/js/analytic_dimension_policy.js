7de2ae7110b4fb6eb6b16b0628a8e99bb51bee5b
Luc De Meyer <luc.demeyer@noviat.com>
2020-11-12 13:37:03 +0100

    [13.0] makeRecord js logic for analytic dimensions

--- account_analytic_dimension_policy/static/src/js/analytic_dimension_policy.js
+++ account_analytic_dimension_policy/static/src/js/analytic_dimension_policy.js
@@ -10,6 +10,7 @@ odoo.define("account_analytic_dimensions_policy.analytic_dimensions_policy", fun
 
     var ReconciliationModel = require("account.ReconciliationModel");
     var ReconciliationRenderer = require("account.ReconciliationRenderer");
+    var relational_fields = require("web.relational_fields");
 
     ReconciliationModel.StatementModel.include({
         reload: function() {
@@ -25,28 +26,90 @@ odoo.define("account_analytic_dimensions_policy.analytic_dimensions_policy", fun
                     ["deprecated", "=", false],
                 ],
             }).then(function(accounts) {
-                self.analytic_dimension_policy_required_account_ids = _.pluck(
+                self.AnalyticDimensionPolicyRequired_account_ids = _.pluck(
                     accounts,
                     "id"
                 );
             });
 
+            this.AnalyticDimensions = [];
+            this.NewAnalyticDimensions = [];
+            this.NewAnalyticDimensionsFields = [];
             var def_dims = self
                 ._rpc({
                     model: "account.move.line",
-                    method: "get_analytic_dimensions",
+                    method: "get_analytic_dimension_fields",
                 })
-                .then(function(dims) {
-                    self.analytic_dimensions = dims;
+                .then(function(result) {
+                    self.AnalyticDimensions = _.pluck(result, "name");
+                    _.each(result, function(field) {
+                        if (!self.quickCreateFields.includes(field.name)) {
+                            self.quickCreateFields.push(field.name);
+                            self.NewAnalyticDimensions.push(field.name);
+                            self.NewAnalyticDimensionsFields.push(field);
+                        }
+                    });
                 });
 
             return Promise.all([def_reload, def_accounts, def_dims]).then(function() {
                 return def_reload;
             });
         },
+
+        _formatQuickCreate: function(line, values) {
+            var prop = this._super(line, values);
+            _.each(this.NewAnalyticDimensions, function(field) {
+                prop[field] = false;
+            });
+            return prop;
+        },
+
+        makeRecord: function(model, fields, fieldInfo) {
+            if (
+                model === "account.bank.statement.line" &&
+                fields.some(field => field.name === "amount")
+            ) {
+                _.each(this.NewAnalyticDimensionsFields, function(field) {
+                    fields.push(field);
+                });
+                _.extend(
+                    fieldInfo,
+                    _.pluck(this.NewAnalyticDimensionsFields, "string")
+                );
+            }
+            return this._super(model, fields, fieldInfo);
+        },
+
+        _formatToProcessReconciliation: function(line, prop) {
+            var result = this._super(line, prop);
+            _.each(this.NewAnalyticDimensions, function(field) {
+                if (prop[field]) {
+                    result[field] = prop[field].id ? prop[field].id : prop[field];
+                }
+            });
+            return result;
+        },
     });
 
     ReconciliationRenderer.LineRenderer.include({
+        _renderCreate: function(state) {
+            var self = this;
+            return Promise.resolve(this._super(state)).then(function() {
+                var record = self.model.get(self.handleCreateRecord);
+                _.each(self.model.NewAnalyticDimensions, function(field) {
+                    self.fields[field] = new relational_fields.FieldMany2One(
+                        self,
+                        field,
+                        record,
+                        {mode: "edit"}
+                    );
+                    self.fields[field].appendTo(
+                        self.$el.find(".create_" + field + " .o_td_field")
+                    );
+                });
+            });
+        },
+
         _onFieldChanged: function(event) {
             var self = this;
             this._super(event);
@@ -55,17 +118,17 @@ odoo.define("account_analytic_dimensions_policy.analytic_dimensions_policy", fun
             if (fieldName === "account_id") {
                 var account_id = event.data.changes.account_id.id;
                 if (
-                    this.model.analytic_dimension_policy_required_account_ids.includes(
+                    this.model.AnalyticDimensionPolicyRequired_account_ids.includes(
                         account_id
                     )
                 ) {
-                    _.each(this.model.analytic_dimensions, function(dim) {
+                    _.each(this.model.AnalyticDimensions, function(dim) {
                         if (self.fields[dim]) {
                             self.fields[dim].$el.addClass("o_required_modifier");
                         }
                     });
                 } else {
-                    _.each(this.model.analytic_dimensions, function(dim) {
+                    _.each(this.model.AnalyticDimensions, function(dim) {
                         if (self.fields[dim]) {
                             self.fields[dim].$el.removeClass("o_required_modifier");
                         }
