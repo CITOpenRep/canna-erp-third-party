aeb415dd1611beb90be2ad2ec3187ef7198a6fa2
Luc De Meyer <luc.demeyer@noviat.com>
2020-12-30 09:41:42 +0100

    [13.0]role_policy - align with latest OCA module

--- role_policy/__manifest__.py
+++ role_policy/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Role Policy",
-    "version": "13.0.0.3.2",
+    "version": "13.0.0.3.4",
     "license": "AGPL-3",
     "author": "Noviat, Odoo Community Association (OCA)",
     "website": "http://www.noviat.com",
