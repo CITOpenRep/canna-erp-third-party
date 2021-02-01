65dbcbdc8cf31856283ef0730a11923e55fbc90b
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2021-02-01 13:57:02 +0530

    [ADD] mis_report_state: Added a state in MIS report

---
+++ mis_report_state/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd.
+#    (<http://www.serpentcs.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "MIS Report State",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "SerpentCS",
+    "website": "http://www.serpentcs.com",
+    "category": "MIS Report",
+    "depends": ["mis_builder"],
+    "data": ["views/mis_report_instance.xml"],
+    "installable": True,
+}
---
+++ mis_report_state/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd.
+#    (<http://www.serpentcs.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import mis_report_instance
