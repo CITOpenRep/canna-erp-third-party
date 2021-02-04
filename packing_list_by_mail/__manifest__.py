f0e533feec4f001449605a5b78ca2782358bb684
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2021-02-04 15:14:02 +0530

    [ADD] Added packing_list_by_mail module for mail a packing list to partners (TAR-0000000536).

---
+++ packing_list_by_mail/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd.
+#    (<http://www.serpentcs.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Packing List By Mail",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "SerpentCS",
+    "website": "http://www.serpentcs.com",
+    "category": "Operations/Purchase",
+    "depends": ["purchase_stock"],
+    "data": ["data/mail_template_data.xml", "views/purchase_views.xml"],
+    "installable": True,
+}
